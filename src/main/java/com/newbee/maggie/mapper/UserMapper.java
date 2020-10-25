65241864793a15f8cf88e81643e8fc8c548b1ab6
Rememorio <1126631928@qq.com>
2020-10-25 14:35:29 +0800

    10.25 验证云数据库连接

--- src/main/java/com/newbee/maggie/mapper/UserMapper.java
+++ src/main/java/com/newbee/maggie/mapper/UserMapper.java
@@ -10,4 +10,11 @@ public interface UserMapper {
      * @return
      */
     User findUserByUsername(String nickname);
+
+    /**
+     * 添加新用户
+     * @param user
+     * @return
+     */
+    int insertUser(User user);
 }
